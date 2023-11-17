6fe9e3f904f86222933d0c39f7b077159d33f3aa
iFrodo <igor@azinet.ru>
2023-11-17 13:09:45 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cards = getCards()
 let user = getCurrentUser()
 renderHeader(user.name)
-renderCards(cards)
+renderCard(cards)
 
 function getCards() {
   let cards = [
@@ -9,25 +9,25 @@ function getCards() {
       type: 'Debit',
       networkType: 'mastercard',
       currencyType: 1,
-      balance: '$5.750,53',
+      balance: '5.750,53',
       number: 5282345678901289,
       expirationYear: 2025,
       expirationMonth: 9,
       transactions: [
         {
           title: 'Order Revenue',
-          amount: 874,
-          date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+          amount: '2354',
+          date: new Date(2022, 3, 25) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
         },
         {
           title: 'Order Revenue',
-          amount: 874,
-          date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+          amount: '6474',
+          date: new Date(2022, 3, 25) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
         },
         {
           title: 'Order Revenue',
-          amount: 0,
-          date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+          amount: '11',
+          date: new Date(2022, 3, 25) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
         },
 
       ],
@@ -38,24 +38,24 @@ function getCards() {
       type: 'Credit',
       networkType: 'visa',
       currencyType: 2,
-      balance: '€1.343,53',
+      balance: '1.343,53',
       number: 9232654321012032,
       expirationYear: 2026,
       expirationMonth: 4,
       transactions: [
         {
           title: 'Order Revenue',
-          amount: 874,
+          amount: '1354',
           date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
         },
         {
           title: 'Order Revenue',
-          amount: 0,
+          amount: '-5601',
           date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
         },
         {
           title: 'Order Revenue',
-          amount: 0,
+          amount: '4321',
           date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
         },
       ]
@@ -65,24 +65,24 @@ function getCards() {
       type: 'Debit',
       networkType: 'master card',
       currencyType: 3,
-      balance: '£3.263,65',
+      balance: '3.263,65',
       number: 5543259349582930,
       expirationYear: 2028,
       expirationMonth: 3,
       transactions: [
         {
           title: 'Order Revenue',
-          amount: 0,
+          amount: '5421',
           date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
         },
         {
           title: 'Order Revenue',
-          amount: 0,
+          amount: '-658',
           date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
         },
         {
           title: 'Order Revenue',
-          amount: 0,
+          amount: '543',
           date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
         },
       ]
@@ -90,55 +90,53 @@ function getCards() {
   ]
   return cards
 }
-function renderCards(cards) {
+function renderCard(cards) {
+  //создание общего контейнера в котором лежат все карты
   document.write('<div class="container">')
   for (let i = 0; i < cards.length; i++) {
-
+    //генерация разметки для каждой карты и подставление данных из входящего массива
     document.write('<div class="card">')
     document.write(`<div class="card-block-${i}">`)
-
     document.write(`<h3 class=card-title>`, cards[i].type, ' Card ', '</h3>')
-    document.write(`<p class=card-balance>`, 'Current Balance', '</p>', `<p class=card-money>`, cards[i].balance, '</p>')
+    if (cards[i].currencyType === 1) {
+      document.write(`<p class=card-balance>`, 'Current Balance', '</p>', `<p class=card-money>`, '$', cards[i].balance, '</p>')
+    } else if (cards[i].currencyType === 2) {
+      document.write(`<p class=card-balance>`, 'Current Balance', '</p>', `<p class=card-money>`, '€', cards[i].balance, '</p>')
+    } else if (cards[i].currencyType === 3) {
+      document.write(`<p class=card-balance>`, 'Current Balance', '</p>', `<p class=card-money>`, '£', cards[i].balance, '</p>')
+    }
+
+
     document.write(`<div class=card-info>`)
     document.write(`<img class=card-logo-${i} src="./mastercard_logo.png" >`)
     document.write(`<p class=card-number>`, cards[i].number, '</p>')
     document.write(`<p class=card-expiration>`, cards[i].expirationYear, '.', cards[i].expirationMonth, '</p>')
     document.write('</div>')
-
     document.write('</div>')
-
     document.write('<div class=transaction-block>')
     document.write("<h2 class='transaction-title'}>", "History Transaction", "</h2>");
     document.write('<ul style=transaction-list>')
-
-    for (let j = 0; j < cards[i].transactions.length; j++) {
-      let cssClass = cards[i].transactions[j].amount > 0 ? "income" : "outcome";
-      document.write("<li class=transaction-item>",
-        cards[i].transactions[j].title,
-        ", ",
-        cards[i].transactions[j].date,
-        ",",
-        `<span class="${cssClass}">`,
-        cards[i].transactions[j].amount,
-        '</span>',
-        "</li>");
-
-    }
+    //рендер транзакций
+    renderTransactions(cards[i].transactions, cards[i].currencyType);
     document.write('</div>')
     document.write('</div>')
-
-    if (cards[i].currencyType === 1) {
-      console.log('EUR');
-    } else if (cards[i].currencyType === 2) {
-      console.log('USD');
-    } else if (cards[i].currencyType === 3) {
-      console.log('Pound sterling')
-    }
   }
   document.write('</ul>')
   document.write('</div>');
+}
+
+function renderTransactions(transactions, currencyType) {
+  if (currencyType === 1) {
+    renderTransactionItem(transactions, '$')
+  } else if (currencyType === 2) {
+    renderTransactionItem(transactions, '€')
+  } else if (currencyType === 3) {
+    renderTransactionItem(transactions, '£')
+  }
+
 
 }
+
 function getCurrentUser() {
   return {
     name: getUserName(),
@@ -146,11 +144,32 @@ function getCurrentUser() {
   };
 
 }
-function getUserName () {
+function getUserName() {
   return 'Igor'
 }
 function renderHeader(userName) {
   document.write("<h1>", "Hello, " + userName, "</h1>");
   document.write("<h2>", "Cards: ", "</h2>");
 
+}
+function renderTransactionItem(transactions, currencyType) {
+  for (let j = 0; j < transactions.length; j++) {
+    let cssClass = transactions[j].amount > 0 ? "income" : "outcome";
+    let sign = ''
+    if(transactions[j].amount > 0){
+      sign = '+'
+    }
+    else if (transactions[j].amount < 0){
+      sign = '-'
+    }
+    document.write("<li class=transaction-item>",
+      transactions[j].title,
+      ", ",
+      transactions[j].date,
+      ",",
+      `<span class="${cssClass}">`,
+      `${sign} ${currencyType}${transactions[j].amount.slice(1)}`,
+      '</span>',
+      "</li>");
+  }
 }
